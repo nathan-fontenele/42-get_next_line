e3502d608343a4d56567d18df990c38851f22383
Nathan Fontenele <nathangf60@outlook.com>
2025-12-11 19:22:43 -0300

    feat: functions prototypes

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,19 @@
 /*   By: agomes-f <agomes-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/11 18:23:13 by agomes-f          #+#    #+#             */
-/*   Updated: 2025/12/11 18:23:14 by agomes-f         ###   ########.fr       */
+/*   Updated: 2025/12/11 19:22:07 by agomes-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_GET_NEXT_LINE_H
+# define FT_GET_NEXT_LINE_H
+
+# include <strlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(char *str);
+char	*ft_strchr(const char *string, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+
+#endif
